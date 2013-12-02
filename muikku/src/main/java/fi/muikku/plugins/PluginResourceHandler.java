6613bb6a503484a6524c351a3dfe8cabc2398060
Antti Leppä <antti.leppa@foyt.fi>
2013-12-02 13:15:18 +0200

    ** Merged PluginResourceResolver into PluginResourceHandler

--- muikku/src/main/java/fi/muikku/plugins/PluginResourceHandler.java
+++ muikku/src/main/java/fi/muikku/plugins/PluginResourceHandler.java
@@ -1,19 +1,22 @@
 package fi.muikku.plugins;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.faces.application.Resource;
 import javax.faces.application.ResourceHandler;
 import javax.faces.application.ResourceHandlerWrapper;
+import javax.faces.application.ViewResource;
 import javax.faces.context.FacesContext;
 
 import fi.muikku.plugin.manager.PluginManagerException;
 import fi.muikku.plugin.manager.SingletonPluginManager;
 
 public class PluginResourceHandler extends ResourceHandlerWrapper {
-	
+
+  private static final String PLUGIN_RESOURCE_BASE_PATH = "META-INF/resources";
 	private Logger logger = Logger.getLogger(PluginResourceHandler.class.getName());
 		
 	public PluginResourceHandler(ResourceHandler resourceHandler) {
@@ -60,6 +63,34 @@ public class PluginResourceHandler extends ResourceHandlerWrapper {
 		} finally {
 		  Thread.currentThread().setContextClassLoader(contextClassLoader);
 		}
+	}
+	
+	@Override
+	public ViewResource createViewResource(FacesContext context, String resourceName) {
+	  final URL resource;
+
+    SingletonPluginManager pluginManager;
+    try {
+      pluginManager = SingletonPluginManager.getInstance();
+
+      ClassLoader pluginsClassLoader = pluginManager.getPluginsClassLoader();
+
+      StringBuilder pluginResourcePath = new StringBuilder().append(PLUGIN_RESOURCE_BASE_PATH).append(resourceName);
+
+      resource = pluginsClassLoader.getResource(pluginResourcePath.toString());
+      if (resource != null) {
+        return new ViewResource() {
+          @Override
+          public URL getURL() {
+            return resource;
+          }
+        };
+      }
+    } catch (PluginManagerException e) {
+      logger.log(Level.SEVERE, "Plugin resource resolving failed", e);
+    }
+
+    return super.createViewResource(context, resourceName);
 	}
 	
 	private final ResourceHandler resourceHandler;
