7584da5ff0a5a670463c43b637eaca34cbe4cb39
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2014-11-21 03:25:20 +0200

    ** Bug fixes

--- muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/OauthClientController.java
+++ muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/OauthClientController.java
@@ -9,47 +9,43 @@ import fi.muikku.plugins.schooldatapyramus.dao.SystemAccessTokenDAO;
 import fi.muikku.plugins.schooldatapyramus.dao.UserAccessTokenDAO;
 import fi.muikku.plugins.schooldatapyramus.model.SystemAccessToken;
 import fi.muikku.plugins.schooldatapyramus.model.UserAccessToken;
-import fi.muikku.plugins.schooldatapyramus.rest.AbstractPyramusClient;
 import fi.muikku.plugins.schooldatapyramus.rest.AccessToken;
 import fi.muikku.session.SessionController;
 
 @Dependent
 public class OauthClientController {
-
+  
   @Inject
   private SystemAccessTokenDAO systemAccessTokenDAO;
 
   @Inject
   private UserAccessTokenDAO userAccessTokenDAO;
-
-  @Inject
-  private AbstractPyramusClient abstractPyramusClient;
   
   @Inject
   private SessionController sessionController;
   
-  public UserAccessToken createUserAccessToken(String authCode) {
-    AccessToken accessToken = abstractPyramusClient.createAccessToken(authCode);
+  public UserAccessToken createUserAccessToken(AccessToken accessToken, String authCode) {
     DateTime accessTokenExpires = new DateTime().plusSeconds(accessToken.getExpiresIn());
     return userAccessTokenDAO.create(accessToken.getAccessToken(), accessTokenExpires.getMillis(), accessToken.getRefreshToken(), sessionController.getLoggedUserEntity().getId(), authCode);
   }
   
-  public SystemAccessToken createSystemAccessToken(String authCode) {
-    AccessToken accessToken = abstractPyramusClient.createAccessToken(authCode);
+  public SystemAccessToken createSystemAccessToken(AccessToken accessToken, String authCode) {
     DateTime accessTokenExpires = new DateTime().plusSeconds(accessToken.getExpiresIn());
     return systemAccessTokenDAO.create(accessToken.getAccessToken(), accessTokenExpires.getMillis(), accessToken.getRefreshToken(), authCode);
   }
   
-  public UserAccessToken refreshUserAccessToken(UserAccessToken userAccessToken){ //TODO: change to use refresh token
-    AccessToken accessToken = abstractPyramusClient.createAccessToken(userAccessToken.getAuthCode());
-    DateTime accessTokenExpires = new DateTime().plusSeconds(accessToken.getExpiresIn());
-    return userAccessTokenDAO.refresh(userAccessToken, accessToken.getAccessToken(), accessTokenExpires.getMillis());
+  public UserAccessToken refreshUserAccessToken(UserAccessToken userAccessToken, AccessToken newAccessToken){ //TODO: change to use refresh token
+    DateTime accessTokenExpires = new DateTime().plusSeconds(newAccessToken.getExpiresIn());
+    return userAccessTokenDAO.refresh(userAccessToken, newAccessToken.getAccessToken(), accessTokenExpires.getMillis());
   }
   
-  public SystemAccessToken refreshSystemAccessToken(SystemAccessToken systemAccessToken){ //TODO: change to use refresh token
-    AccessToken accessToken = abstractPyramusClient.createAccessToken(systemAccessToken.getAuthCode());
-    DateTime accessTokenExpires = new DateTime().plusSeconds(accessToken.getExpiresIn());
-    return systemAccessTokenDAO.refresh(systemAccessToken, accessToken.getAccessToken(), accessTokenExpires.getMillis());
+  public SystemAccessToken refreshSystemAccessToken(SystemAccessToken systemAccessToken, AccessToken newAccessToken){ //TODO: change to use refresh token
+    DateTime accessTokenExpires = new DateTime().plusSeconds(newAccessToken.getExpiresIn());
+    return systemAccessTokenDAO.refresh(systemAccessToken, newAccessToken.getAccessToken(), accessTokenExpires.getMillis());
+  }
+  
+  public SystemAccessToken findSystemAccessToken(){
+    return systemAccessTokenDAO.getSystemAccessToken();
   }
   
   public boolean isExpired(UserAccessToken userAccessToken){
