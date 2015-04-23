54ec4eeeeb2372a19dde883e59c2328c8f93215c
ilmo.euro <ilmo.euro@gmail.com>
2015-04-23 15:53:10 +0300

    ** Added null check when retrieving courses from Pyramus

--- muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/PyramusWorkspaceSchoolDataBridge.java
+++ muikku-pyramus-plugin/src/main/java/fi/muikku/plugins/schooldatapyramus/PyramusWorkspaceSchoolDataBridge.java
@@ -29,13 +29,13 @@ import fi.pyramus.rest.model.Subject;
 @Dependent
 @Stateful
 public class PyramusWorkspaceSchoolDataBridge implements WorkspaceSchoolDataBridge {
-  
+
   @Inject
   private PyramusClient pyramusClient;
 
   @Inject
   private PyramusIdentifierMapper identifierMapper;
-  
+
   @Inject
   private PyramusSchoolDataEntityFactory entityFactory;
 
@@ -44,144 +44,168 @@ public class PyramusWorkspaceSchoolDataBridge implements WorkspaceSchoolDataBrid
     return SchoolDataPyramusPluginDescriptor.SCHOOL_DATA_SOURCE;
   }
 
-	@Override
-	public Workspace createWorkspace(String name, String description, WorkspaceType type, String courseIdentifierIdentifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
-		if (StringUtils.isBlank(name)) {
-			throw new SchoolDataBridgeRequestException("Name is required");
-		}
-		
-		if (name.length() > 255) {
-			throw new SchoolDataBridgeRequestException("Name maximum length is 255 characters");
-		}
-		
-		throw new UnexpectedSchoolDataBridgeException("Not implemented");
-	}
+  @Override
+  public Workspace createWorkspace(String name, String description, WorkspaceType type,
+      String courseIdentifierIdentifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
+    if (StringUtils.isBlank(name)) {
+      throw new SchoolDataBridgeRequestException("Name is required");
+    }
 
-	@Override
-	public Workspace findWorkspace(String identifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
-		if (!StringUtils.isNumeric(identifier)) {
-			throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
-		}
+    if (name.length() > 255) {
+      throw new SchoolDataBridgeRequestException("Name maximum length is 255 characters");
+    }
 
-    return createWorkspaceEntity(pyramusClient.get("/courses/courses/" + identifier, Course.class));
-	}
+    throw new UnexpectedSchoolDataBridgeException("Not implemented");
+  }
 
   @Override
-	public List<Workspace> listWorkspaces() throws UnexpectedSchoolDataBridgeException {
+  public Workspace findWorkspace(String identifier) throws SchoolDataBridgeRequestException,
+      UnexpectedSchoolDataBridgeException {
+    if (!StringUtils.isNumeric(identifier)) {
+      throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
+    }
+    
+    Course course = pyramusClient.get("/courses/courses/" + identifier, Course.class);
+    
+    if (course == null) {
+      return null;
+    } else {
+        return createWorkspaceEntity(course);
+    }
+  }
+
+  @Override
+  public List<Workspace> listWorkspaces() throws UnexpectedSchoolDataBridgeException {
     Course[] courses = pyramusClient.get("/courses/courses/", Course[].class);
     if (courses == null) {
       throw new UnexpectedSchoolDataBridgeException("Null response");
     }
-    
+
     List<Workspace> result = new ArrayList<Workspace>();
-    
+
     for (Course course : courses) {
       result.add(createWorkspaceEntity(course));
     }
-    
-    return result;
-	}
 
-	@Override
-	public List<Workspace> listWorkspacesByCourseIdentifier(String courseIdentifierIdentifier) throws UnexpectedSchoolDataBridgeException {
+    return result;
+  }
+
+  @Override
+  public List<Workspace> listWorkspacesByCourseIdentifier(String courseIdentifierIdentifier)
+      throws UnexpectedSchoolDataBridgeException {
     if (courseIdentifierIdentifier.indexOf("/") == -1)
       throw new UnexpectedSchoolDataBridgeException("Invalid CourseIdentifierId");
-    
+
     String subjectId = courseIdentifierIdentifier.substring(0, courseIdentifierIdentifier.indexOf("/"));
     String courseNumber = courseIdentifierIdentifier.substring(courseIdentifierIdentifier.indexOf("/") + 1);
-    
-    Course[] courses = pyramusClient.get("/common/subjects/" + subjectId + "/courses", fi.pyramus.rest.model.Course[].class);
+
+    Course[] courses = pyramusClient.get("/common/subjects/" + subjectId + "/courses",
+        fi.pyramus.rest.model.Course[].class);
     List<Workspace> result = new ArrayList<Workspace>();
-    
+
     for (Course course : courses) {
       String courseNumber2 = course.getCourseNumber() != null ? course.getCourseNumber().toString() : "null";
 
       if (courseNumber.equals(courseNumber2))
         result.add(createWorkspaceEntity(course));
     }
-    
+
     return result;
-	}
-
-	@Override
-	public Workspace updateWorkspace(Workspace workspace) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
-		if (!StringUtils.isNumeric(workspace.getIdentifier())) {
-			throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
-		}
-		
-		throw new UnexpectedSchoolDataBridgeException("Not implemented");
-	}
-
-	@Override
-	public void removeWorkspace(String identifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
-		if (!StringUtils.isNumeric(identifier)) {
-			throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
-		}
-
-		throw new UnexpectedSchoolDataBridgeException("Not implemented");
-	}
-
-	@Override
-	public WorkspaceType findWorkspaceType(String identifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
-		if (identifier == null) {
-		  return null;
-		}
-	  
-	  if (!StringUtils.isNumeric(identifier)) {
-			throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
-		}
-		
-		return entityFactory.createEntity(pyramusClient.get("/courses/courseTypes/" + identifier, fi.pyramus.rest.model.CourseType.class));
   }
-	
-	@Override
-	public List<WorkspaceType> listWorkspaceTypes() throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
-    return entityFactory.createEntities(pyramusClient.get("/courses/courseTypes/", fi.pyramus.rest.model.CourseType[].class));
-	}
 
   @Override
-  public WorkspaceUser createWorkspaceUser(Workspace workspace, User user, String roleSchoolDataSource, String roleIdentifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
+  public Workspace updateWorkspace(Workspace workspace) throws SchoolDataBridgeRequestException,
+      UnexpectedSchoolDataBridgeException {
+    if (!StringUtils.isNumeric(workspace.getIdentifier())) {
+      throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
+    }
+
+    throw new UnexpectedSchoolDataBridgeException("Not implemented");
+  }
+
+  @Override
+  public void removeWorkspace(String identifier) throws SchoolDataBridgeRequestException,
+      UnexpectedSchoolDataBridgeException {
+    if (!StringUtils.isNumeric(identifier)) {
+      throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
+    }
+
+    throw new UnexpectedSchoolDataBridgeException("Not implemented");
+  }
+
+  @Override
+  public WorkspaceType findWorkspaceType(String identifier) throws SchoolDataBridgeRequestException,
+      UnexpectedSchoolDataBridgeException {
+    if (identifier == null) {
+      return null;
+    }
+
+    if (!StringUtils.isNumeric(identifier)) {
+      throw new SchoolDataBridgeRequestException("Identifier has to be numeric");
+    }
+
+    return entityFactory.createEntity(pyramusClient.get("/courses/courseTypes/" + identifier,
+        fi.pyramus.rest.model.CourseType.class));
+  }
+
+  @Override
+  public List<WorkspaceType> listWorkspaceTypes() throws SchoolDataBridgeRequestException,
+      UnexpectedSchoolDataBridgeException {
+    return entityFactory.createEntities(pyramusClient.get("/courses/courseTypes/",
+        fi.pyramus.rest.model.CourseType[].class));
+  }
+
+  @Override
+  public WorkspaceUser createWorkspaceUser(Workspace workspace, User user, String roleSchoolDataSource,
+      String roleIdentifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
     Long courseId = identifierMapper.getPyramusCourseId(workspace.getIdentifier());
     Long studentId = identifierMapper.getPyramusStudentId(user.getIdentifier());
-    
-    CourseStudent courseStudent = new CourseStudent(null, courseId, studentId, new DateTime(), Boolean.FALSE, null, null, Boolean.FALSE, CourseOptionality.OPTIONAL, null);
-    
-    return Arrays.asList(entityFactory.createEntity(pyramusClient.post("/courses/courses/" + courseId + "/students", courseStudent))).get(0);
+
+    CourseStudent courseStudent = new CourseStudent(null, courseId, studentId, new DateTime(), Boolean.FALSE, null,
+        null, Boolean.FALSE, CourseOptionality.OPTIONAL, null);
+
+    return Arrays.asList(
+        entityFactory.createEntity(pyramusClient.post("/courses/courses/" + courseId + "/students", courseStudent)))
+        .get(0);
   }
-  
-	@Override
-	public WorkspaceUser findWorkspaceUser(String identifier) throws UnexpectedSchoolDataBridgeException {
-	  throw new UnexpectedSchoolDataBridgeException("Not implemented");
-	}
-	
-	@Override
-	public List<WorkspaceUser> listWorkspaceUsers(String workspaceIdentifier) throws SchoolDataBridgeRequestException, UnexpectedSchoolDataBridgeException {
-	  Long courseId = identifierMapper.getPyramusCourseId(workspaceIdentifier);
-	  
-	  CourseStaffMember[] staffMembers = pyramusClient.get("/courses/courses/" + courseId + "/staffMembers", CourseStaffMember[].class);
+
+  @Override
+  public WorkspaceUser findWorkspaceUser(String identifier) throws UnexpectedSchoolDataBridgeException {
+    throw new UnexpectedSchoolDataBridgeException("Not implemented");
+  }
+
+  @Override
+  public List<WorkspaceUser> listWorkspaceUsers(String workspaceIdentifier) throws SchoolDataBridgeRequestException,
+      UnexpectedSchoolDataBridgeException {
+    Long courseId = identifierMapper.getPyramusCourseId(workspaceIdentifier);
+
+    CourseStaffMember[] staffMembers = pyramusClient.get("/courses/courses/" + courseId + "/staffMembers",
+        CourseStaffMember[].class);
     if (staffMembers == null) {
       throw new UnexpectedSchoolDataBridgeException("Null response");
     }
-    
-    CourseStudent[] courseStudents = pyramusClient.get("/courses/courses/" + courseId + "/students", CourseStudent[].class);
+
+    CourseStudent[] courseStudents = pyramusClient.get("/courses/courses/" + courseId + "/students",
+        CourseStudent[].class);
     if (courseStudents == null) {
       throw new UnexpectedSchoolDataBridgeException("Null response");
     }
-    
+
     List<WorkspaceUser> result = entityFactory.createEntity(staffMembers);
     result.addAll(entityFactory.createEntity(courseStudents));
-    
+
     return result;
-	}
-  
+  }
+
   private Workspace createWorkspaceEntity(Course course) {
     String educationTypeIdentifier = null;
-    
+
     if (course.getSubjectId() != null) {
-      Subject subject = pyramusClient.get("/common/subjects/" + course.getSubjectId(), fi.pyramus.rest.model.Subject.class);
+      Subject subject = pyramusClient.get("/common/subjects/" + course.getSubjectId(),
+          fi.pyramus.rest.model.Subject.class);
       educationTypeIdentifier = identifierMapper.getEducationTypeIdentifier(subject.getEducationTypeId());
     }
-    
+
     return entityFactory.createEntity(course, educationTypeIdentifier);
   }
 }
