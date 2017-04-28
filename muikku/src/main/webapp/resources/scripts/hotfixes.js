9215ee2e7572858fe0f733cf6507497b9fbe9d84
Felishia Gonzalez <ejgonzalezviera@gmail.com>
2017-04-28 12:50:08 +0300

    added hotfixes javascript file

---
+++ muikku/src/main/webapp/resources/scripts/hotfixes.js
@@ -0,0 +1,13 @@
+// BUG-2792_connect-field-draggable-issue
+// This should be fixed as more development goes to the new ui design
+// Don't leave this here after all draggables and droppables are fixed
+// currently they don't work on touch devices and instead block the scrolling
+// so we disable them overall until they are fixed
+if ('ontouchstart' in window || 'onmsgesturechange' in window){
+  $.fn.draggable = function(){
+    return this;
+  }
+  $.fn.droppable = function(){
+    return this;
+  }
+}
