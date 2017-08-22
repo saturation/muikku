ea55378daa26c6af995f172530486daaa8362eec
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2017-08-22 09:28:32 +0300

    Fixed loading of course status

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-courselist.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-courselist.js
@@ -1,39 +1,37 @@
  
 $(document).ready(function(){
 
-    mApi({async: false}).workspace.workspaces
+  mApi().workspace.workspaces
     .read({ userId: MUIKKU_LOGGED_USER_ID })
-    .on('$', $.proxy(function (workspace, workspaceCallback){
+    .on('$', $.proxy(function (workspace, workspaceCallback) {
       var workspaceId = workspace.id;
       
       mApi().guider.workspaces.activity
-      .cacheClear()
-      .read(workspaceId)
-      .callback($.proxy(function (err, activity) {         
-        workspace.donePercent = activity.evaluablesDonePercent;
-      }));
-      workspaceCallback();
+        .cacheClear()
+        .read(workspaceId)
+        .callback($.proxy(function (err, activity) {         
+          workspace.donePercent = activity.evaluablesDonePercent;
+          workspaceCallback();
+        }));
     },this))
     .callback( function (err, workspaces) {
-      
-    	if( err ){
-            $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
-    	}else{    	  
-    	  renderDustTemplate('frontpage/widget_workspaces.dust', {
-        	workspaces:workspaces
+      if (err) {
+        $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
+      } else {        
+        renderDustTemplate('frontpage/widget_workspaces.dust', {
+          workspaces: workspaces
         }, function (text) {
-            $('#workspaces').append($.parseHTML(text));
-            var is_xs = $(window).width() < 769;
-            if (!is_xs) {
-              $('#workspaces').perfectScrollbar({
-                wheelSpeed:3,
-                swipePropagation:false,
-                wheelPropagation:true
-              });
-            }
-          });
-        
-    	}
+          $('#workspaces').append($.parseHTML(text));
+          var is_xs = $(window).width() < 769;
+          if (!is_xs) {
+            $('#workspaces').perfectScrollbar({
+              wheelSpeed: 3,
+              swipePropagation: false,
+              wheelPropagation: true
+            });
+          }
+        });
+      }
     });
 });
 
