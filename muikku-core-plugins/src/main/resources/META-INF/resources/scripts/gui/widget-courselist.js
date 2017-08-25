762304f7536d61fefb38feee425217721ab84ba9
Antti Ukkonen <antti.ukkonen@otavanopisto.fi>
2017-08-25 14:15:35 +0300

    Static workspace order sorting in javascript

--- muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-courselist.js
+++ muikku-core-plugins/src/main/resources/META-INF/resources/scripts/gui/widget-courselist.js
@@ -5,15 +5,44 @@ $(document).ready(function(){
     .read({ userId: MUIKKU_LOGGED_USER_ID })
     .on('$', $.proxy(function (workspace, workspaceCallback){
       var workspaceId = workspace.id; 
+      
       mApi().guider.workspaces.activity
       .cacheClear()
       .read(workspaceId)
       .callback($.proxy(function (err, activity) {         
         workspace.donePercent = activity.evaluablesDonePercent;
-        workspaceCallback();
+        
+        if (workspace.urlName === "tervetuloa-startup-high-shooliin"){
+          workspace.indexNo = 0;
+        }
+        else if (workspace.urlName === "su1-startup-yrittajan-ajattelun-taidot"){
+          workspace.indexNo = 1;          
+        }
+        else if (workspace.urlName === "su2-startup-yrittajan-tietopaketti"){
+          workspace.indexNo = 2;          
+        }
+        else if (workspace.urlName === "su3-startup-yrittajan-projektikurssi"){
+          workspace.indexNo = 3;          
+        }        
+        else {
+          workspace.indexNo = 5;          
+        }
+        workspaceCallback();        
       }));
     },this))
     .callback( function (err, workspaces) { 
+       
+      function compare(a, b) {
+        if (a.indexNo < b.indexNo) {
+          return -1;
+        }
+        if (a.indexNo > b.indexNo) {
+          return 1;
+        }
+        return 0;
+      }
+      
+      workspaces.sort(compare);
       
     	if( err ){
             $('.notification-queue').notificationQueue('notification', 'error', getLocaleText('TODO: Virheilmoitus', err));
